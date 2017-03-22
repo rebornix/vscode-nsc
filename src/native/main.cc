caa418515daa95117db951070f097f2be7e13977
rebornix <penn.lv@gmail.com>
2017-03-22 16:22:05 -0700

    Windows

--- src/native/main.cc
+++ src/native/main.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <stdio.h>
-#include "util.h"
 #include "node-spellchecker/spellchecker.h"
 
 using namespace spellchecker;
@@ -13,7 +12,7 @@ int main(int argc, const char *argv[])
     std::clog << "ac <word> <word> - Add autocorrect pair" << std::endl;
     std::clog << "ign <word> - Ignore word" << std::endl;
     std::clog << "chkb <text> - Check text (batch - pasted text or file open)" << std::endl;
-    std::clog << "chk <text> - Check text (as you type)" << std::endl;
+	std::clog << "dicset <locale> - Set a custom dictionary" << std::endl;
 
     SpellcheckerImplementation *impl = SpellcheckerFactory::CreateSpellchecker();
     while (true)
@@ -21,8 +20,15 @@ int main(int argc, const char *argv[])
         std::clog << "> ";
         std::string line;
         std::getline(std::cin, line);
-        std::size_t found = line.find_first_of(" ");
-        std::string command = line.substr(0, found);
+        std::size_t separator = line.find_first_of(" ");
+
+		std::string command;
+		if (separator == std::string::npos) {
+			command = line;
+		}
+		else {
+			command = line.substr(0, separator);
+		}
 
         const size_t lineSize = line.length();
         const size_t cmdSize = command.length();
@@ -39,9 +45,22 @@ int main(int argc, const char *argv[])
         }
         else
         {
-            if (command.compare("chkb") == 0)
+			if (command.compare("dicset") == 0) {
+				std::string locale = line.substr(separator + 1);
+				bool setDictionary = impl->SetDictionary(locale);
+				if (setDictionary) {
+					std::clog << "Set dictionary " << locale << std::endl;
+				}
+				else {
+					std::cerr << "No dictionary found for locale " << locale << std::endl;
+				}
+			} else if (command.compare("add") == 0) {
+				std::string word = line.substr(separator + 1);
+				impl->Add(word);
+				std::clog << "Add word " << word << std::endl;
+			} else if (command.compare("chkb") == 0)
             {
-                std::string text = line.substr(found);
+                std::string text = line.substr(separator + 1);
                 std::vector<uint16_t> uiText((text.size() + 1));
                 std::copy_n(text.data(), text.size(), reinterpret_cast<uint16_t *>(uiText.data()));
                 std::vector<MisspelledRange> misspelled_ranges = impl->CheckSpelling(uiText.data(), uiText.size());
